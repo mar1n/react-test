0067192bf32b1aedfbe54d28b6dd96982d705303
mar1n <30738216+mar1n@users.noreply.github.com>
2020-02-16 15:56:09 +0000

    hook check password confirm

--- src/components/medium/hooks/PasswordChecker.js
+++ src/components/medium/hooks/PasswordChecker.js
@@ -1,10 +1,29 @@
 import React, { useState } from 'react';
 
 export default function PasswordChecker() {
-    const [password, setPassword] = useState();
+    const [pwd, setPwd] = useState('');
+    const [confirmpwd, setConfirmpwd] = useState('');
     return(
         <>
-
+            <input
+                type='password'
+                className='pwd'
+                value={pwd}
+                onChange={(e) => setPwd(e.target.value)}
+            />
+            <input
+                type='password'
+                className='confirmPwd'
+                value={confirmpwd}
+                onChange={(e) => setConfirmpwd(e.target.value)}
+            />
+            <label>
+                {
+                    pwd === confirmpwd
+                    ? 'Passwords match'
+                    : 'Passwords do not match'
+                }
+            </label>
         </>
     )
 }
--- src/components/medium/hooks/PasswordChecker.test.js
+++ src/components/medium/hooks/PasswordChecker.test.js
@@ -11,5 +11,22 @@ import { shallow, mount, render } from 'enzyme';
 describe('Password checker', () => {
     it('PasswordsMatch check if the password is entered correctly', () => {
         
+        const wrapper = mount(<PasswordChecker />);
+        const isStatelessComponent = !wrapper.instance();
+
+        const pwd = wrapper.find('input[type="password"].pwd');
+        const confirmPwd = wrapper.find('input[type="password"].confirmPwd');
+        const label = wrapper.find('label');
+
+        expect(isStatelessComponent).to.equal(true);
+
+        pwd.simulate('change', { target: { value: 'a' } });
+        confirmPwd.simulate('change', { target: { value: 'b' } });
+
+        expect(label.text()).to.equal('Passwords do not match');
+
+        confirmPwd.simulate('change', { target: {value: 'a'}});
+
+        expect(label.text()).to.equal('Passwords match')
     })
  });
